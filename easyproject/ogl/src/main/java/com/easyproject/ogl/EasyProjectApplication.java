4476b3d76d80cd619ee80b3be840252bc5f6b334
Enzo Lima <enzolima527@gmail.com>
2025-03-31 13:12:02 -0300

    .

---
+++ easyproject/ogl/src/main/java/com/easyproject/ogl/EasyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.easyproject.ogl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyProjectApplication.class, args);
+	}
+
+}
